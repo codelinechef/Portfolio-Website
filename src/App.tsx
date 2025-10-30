216aa79ec804a04b20649af1b254d25097f4ae91
codelinechef <asindia23@gmail.com>
2025-10-30 16:02:20 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ const App = () => {
                 <Toaster />
                 <Sonner />
                 <SmoothScroll>
-                  <BrowserRouter>
+                  <BrowserRouter basename={import.meta.env.BASE_URL}>
                     <Routes>
                       <Route path="/" element={<Index />} />
                       <Route path="/fyi" element={<RedZone />} />
